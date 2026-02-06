0f20acb96c8db47ae221329517cd44ae5c79a8c4
Luoi Hoc Ham Choi <142866066+vietTQ123@users.noreply.github.com>
2026-02-06 08:54:30 +0700

    Add files via upload

---
+++ 25.DHT_11/src/DHT_11.h
@@ -0,0 +1,14 @@
+#ifndef __DHT_11__
+#define __DHT_11__
+
+#include "DHT.h"
+#include "Adafruit_Sensor.h"
+
+#define PIN_DHT_11 26
+#define TYPE_DHT_11 DHT11
+
+void DHT11_Setup(void);
+int DHT11_Temperature(void);
+int DHT11_Humidity(void);
+
+#endif
